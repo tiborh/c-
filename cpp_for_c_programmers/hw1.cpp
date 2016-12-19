d439233320cc52bcd60872182acf7d945f7850c3
Tibor Harcsa <tibor@harcsa.net>
2016-12-19 16:30:24 +0100

    fine-tuning homework assignment

--- cpp_for_c_programmers/hw1.cpp
+++ cpp_for_c_programmers/hw1.cpp
@@ -13,31 +13,45 @@
 // change array to vector<>               vector<int> data(N); vector.at(i)
 // inline any short function              inline void sum(int& p, ...
 //
+// Additional changes:
+// =========================================================================
+// names of the variables have been change to make them more meaningful
+// iteration variable i is encapsulated in the for loops now
+// modifications in sum function
+//    introductory comments have been added
+//    generic to practise that material too
+//    no side effect but a returned value
+//    taking an immutable vector
+//
 #include <vector>
 #include <iostream>
 
 using namespace std;
 
-inline void sum(int& p, int n, vector<int> d)
-{
-  int i;
-  p = 0;
+// to sum the elements of a vector
+// the_sum: the sum of the vector elements
+// the_data: the vector storing the elements to sum
+// returns the sum of elements. No side effect
+template<class T>
+inline T sum(T the_sum, const vector<T> the_data) {
+  the_sum = 0;
+  int data_length = the_data.size();
+  
+  for(int i = 0; i < data_length; ++i)
+    the_sum += the_data.at(i);
 
-  for(i = 0; i < n; ++i)
-    p = p + d.at(i);
+  return(the_sum);
 }
 
-int main()
-{
-  const int N = 40;
-  int i;
+int main() {
+  const int NUM_OF_DATA = 40;
   int accum = 0;
-  vector<int> data(N);
+  vector<int> data(NUM_OF_DATA);
 
-  for(i = 0; i < N; ++i)
+  for(int i = 0; i < NUM_OF_DATA; ++i)
     data.at(i) = i;
 
-  sum(accum, N, data);
+  accum = sum<int>(accum, data);
 
   cout << "sum is " << accum << endl;
 
