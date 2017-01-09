10b0a2c14c1b521d6f9ae417eeaa69e8a92c6392
tiborh <tibor@harcsa.net>
2017-01-10 00:55:34 +0100

    solving problem with ambiguity

--- greater.cpp
+++ greater.cpp
@@ -11,36 +11,40 @@ struct rational {
     cout << "rational, type cast to double\n";
     return static_cast<double>(a)/q;
   }
+  friend bool operator>(rational w, rational z);
   friend ostream& operator<<(ostream& os, const rational& other);
 private:
   long a,q;
   enum { BIG = 100 };
 };
 
+bool operator>(rational w, rational z) { 
+   return (static_cast<double>(w.a) / w.q > static_cast<double>(z.a) / z.q); 
+}
+
 ostream& operator<<(ostream& os, const rational& other) {
   os << other.a << " / " << other.q;
   return os;
 }
 
-// if used this way, calls in main are ambiguous
-// inline int greater(int i, int j) {
-//   cout << "greater(int,int) has been called\n";
-//   return ( i > j ? i : j );
-// }
-// inline double greater(double x, double y) {
-//   cout << "greater(double,double) has been called\n";
-//   return ( x > y ? x : y );
-// }
-// inline rational greater(rational w, rational z) {
-//   cout << "greater(rational,rational) has been called\n";
-//   return ( w > z ? w : z );
-// }
-
-template<class T>
-T greater(const T& a, const T& b) {
-  T result = a > b ? a : b;
-  return result;
+inline int greater_than(int i, int j) {
+  cout << "greater_than(int,int) has been called\n";
+  return ( i > j ? i : j );
 }
+inline double greater_than(double x, double y) {
+  cout << "greater_than(double,double) has been called\n";
+  return ( x > y ? x : y );
+}
+inline rational greater_than(rational w, rational z) {
+  cout << "greater_than(rational,rational) has been called\n";
+  return ( w > z ? w : z );
+}
+
+// alternatively:
+// template<class T>
+// T greater_than(const T& a, const T& b) {
+//   return a > b ? a : b;
+// }
 
 int main() {
   int i = 10, j = 5;
@@ -48,12 +52,18 @@ int main() {
   double y = 14.5;
   rational w(10), z(3.5), zmax;
 
-  // won't compile: ambiguous reference
-  cout << "\ngreater(" << i << ", " << j << ") == " << greater<int>(i,j) << '\n';
-  cout << "\ngreater(" << x << ", " << y << ") == " << greater<double>(x,y) << '\n';
-  cout << "\ngreater(" << i << ", " << z << ") == " << greater<rational>(static_cast<rational>(i),z) << '\n';
-  zmax = greater<rational>(w,z);
-  cout << "\ngreater(" << w << ", " << z << ") == " << zmax << '\n';
+  // for template:
+  // cout << "\ngreater_than(" << i << ", " << j << ") == " << greater_than<int>(i,j) << '\n';
+  // cout << "\ngreater_than(" << x << ", " << y << ") == " << greater_than<double>(x,y) << '\n';
+  // cout << "\ngreater_than(" << i << ", " << z << ") == " << greater_than<rational>(static_cast<rational>(i),z) << '\n';
+  // zmax = greater_than<rational>(w,z);
+  // cout << "\ngreater_than(" << w << ", " << z << ") == " << zmax << '\n';
+
+  cout << "\ngreater_than(" << i << ", " << j << ") == " << greater_than(i,j) << '\n';
+  cout << "\ngreater_than(" << x << ", " << y << ") == " << greater_than(x,y) << '\n';
+  cout << "\ngreater_than(" << i << ", " << z << ") == " << greater_than(static_cast<rational>(i),z) << '\n';
+  zmax = greater_than(w,z);
+  cout << "\ngreater_than(" << w << ", " << z << ") == " << zmax << '\n';
   
   return 0;
 }
