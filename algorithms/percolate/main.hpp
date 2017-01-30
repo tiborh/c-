690197449ff1f91595f9ea51d79fd0246d95e641
Tibor Harcsa <silurust@gmail.com>
2017-01-30 01:59:59 +0100

    first of percolate stats.

--- algorithms/percolate/main.hpp
+++ algorithms/percolate/main.hpp
@@ -1,11 +1,10 @@
 #ifndef MAIN_H_INCLUDED
 #define MAIN_H_INCLUDED
 
-#include "Percolation.hpp"
-
 void simple_tests();
 void percolate_tests();
 void random_open_percolate_test();
+void percolate_stat_tests();
 
 #endif //MAIN_H_INCLUDED
 
