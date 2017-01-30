1a0b78a4a765c8f80cc77078e57947ed006060aa
Tibor Harcsa <silurust@gmail.com>
2017-01-30 02:02:18 +0100

    Percolation simulation statistics. (trials and mean)

---
+++ algorithms/percolate/PercolationStats.cpp
@@ -0,0 +1,29 @@
+#include "PercolationStats.hpp"
+#include "Percolation.hpp"
+
+#include <random>
+#include <ctime>
+
+PercolationStats::PercolationStats(unsigned int n, int trials) {
+  tv = std::vector<unsigned int>(trials);
+  std::uniform_int_distribution<unsigned> u(0,n-1);
+  std::default_random_engine e(time(0));
+  for (int i = 0; i < trials; ++i) {
+    Percolation a = Percolation(n,n);
+    while(!a.percolates()) {
+      unsigned int row = u(e);
+      unsigned int col = u(e);
+      //cout << a << '\n';
+      if (a.isBlocked(row,col))
+	a.open(row,col);
+    }
+    tv.at(i) = a.numberOfOpenedSites();
+  }
+}
+
+double PercolationStats::mean() {
+  unsigned long long int sum_of_percs = 0;
+  for(auto it = tv.begin(); it != tv.end(); ++it)
+    sum_of_percs += *it;
+  return sum_of_percs / tv.size();
+}
