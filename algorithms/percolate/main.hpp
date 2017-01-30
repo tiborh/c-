9697a338f7d7974bdb7fbe26d3be1726d12c0734
Tibor Harcsa <silurust@gmail.com>
2017-01-30 01:03:53 +0100

    header file for main.cpp, having function definitions.

---
+++ algorithms/percolate/main.hpp
@@ -0,0 +1,14 @@
+#ifndef MAIN_H_INCLUDED
+#define MAIN_H_INCLUDED
+
+#include "Percolation.hpp"
+
+void simple_tests();
+void percolate_tests();
+void random_open_percolate_test();
+
+#endif //MAIN_H_INCLUDED
+
+// Local Variables:
+// mode: c++
+// End:
