368f1d8bfbb987b51c54b8975ebe2d697f343ec9
Tibor Harcsa <silurust@gmail.com>
2017-01-30 01:02:53 +0100

    Main function for the test of Percolation.

---
+++ algorithms/percolate/main.cpp
@@ -0,0 +1,119 @@
+#include <iostream>
+#include <random>
+#include <ctime>
+#include "main.hpp"
+
+using namespace std;
+
+int main() {
+  cout << "\nSimple tests:\n";
+  cout << "===============\n";
+  simple_tests();
+  cout << "\nBasic Percolate tests:\n";
+  cout << "========================\n";
+  percolate_tests();
+  cout << "\nRandom Open Percolate test:\n";
+  cout << "=============================\n";
+  random_open_percolate_test();
+  return 0;
+}
+
+void random_open_percolate_test() {
+  int grid_size = 10;
+  int nu_of_sites = grid_size * grid_size;
+  Percolation a = Percolation(grid_size,grid_size);
+  uniform_int_distribution<uvnsigned> u(0,grid_size-1);
+  default_random_engine e(time(0));
+  while(!a.percolates() && a.numberOfOpenedSites() <= nu_of_sites) {
+    unsigned int row = u(e);
+    unsigned int col = u(e);
+    //cout << a << '\n';
+    if (a.isBlocked(row,col))
+	a.open(row,col);
+  }
+  cout << "number of opened sites: " << a.numberOfOpenedSites() << '\n';
+  cout << a << '\n';
+}
+
+void simple_tests() {
+  Percolation a = Percolation();
+  cout << a << '\n';
+  assert(!a.percolates());
+  assert(a.numberOfOpenedSites() == 0);
+  Percolation b = Percolation(5,10);
+  assert(!b.isOpen(4,9));
+  assert(!b.isFilled(4,9));
+  assert(b.isBlocked(4,9));
+  b.open(4,9);
+  assert(b.isOpen(4,9));
+  assert(!b.isFilled(4,9));
+  assert(!b.isBlocked(4,9));
+  assert(b.numberOfOpenedSites() == 1);
+  assert(!b.percolates());
+  
+  cout << b << '\n';
+  Percolation c = Percolation(10,5);
+  cout << c << '\n';
+  assert(!c.percolates());
+}
+
+void percolate_tests() {
+  Percolation a = Percolation(1);
+  assert(!a.percolates());
+  a.open(0,0);
+  cout << a << '\n';
+  assert(a.percolates());
+  assert(a.isFilled(0,0));
+  cout << a << '\n';
+
+  Percolation b = Percolation(2);
+  assert(!b.percolates());
+  b.open(0,0);
+  assert(!b.percolates());
+  cout << b << '\n';
+  b.open(1,0);
+  cout << b << '\n';
+  assert(b.percolates());
+  assert(b.isFilled(1,0));
+  cout << b << '\n';
+
+  Percolation c = Percolation(2,2);
+  assert(!c.percolates());
+  c.open(0,0);
+  c.open(1,1);
+  cout << c << '\n';
+  assert(!c.percolates());
+  c.open(0,1);
+  cout << c << '\n';
+  assert(c.percolates());
+  cout << c << '\n';
+
+  Percolation d = Percolation(3,3);
+  d.open(2,2);
+  d.open(1,2);
+  d.open(1,1);
+  d.open(1,0);
+  assert(!d.percolates());
+  cout << d << '\n';
+  d.open(0,0);
+  assert(d.percolates());
+  cout << d << '\n';
+
+  Percolation e = Percolation(4,4);
+  e.open(0,1);
+  assert(!e.percolates());
+  e.open(1,2);
+  assert(!e.percolates());
+  e.open(1,1);
+  assert(!e.percolates());
+  e.open(2,1);
+  assert(!e.percolates());
+  e.open(2,2);
+  assert(!e.percolates());
+  e.open(2,3);
+  assert(!e.percolates());
+  e.open(3,3);
+  assert(e.percolates());
+  cout << e << '\n';
+  assert(e.numberOfOpenedSites() == 7);
+}
