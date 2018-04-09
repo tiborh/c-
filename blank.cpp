b24118d259d2269e7c178c4fdc371dd77f95a63e
tibor harcsa <tibor@harcsa.net>
2018-04-09 04:50:18 +0200

    getting starter template into good shape

--- blank.cpp
+++ blank.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
+#include <vector>		// for most common array substitute
+#include <string>		// for c++ string
+#include <fstream>		// for file operations
 
 using namespace std;
 
-int main() {
+int main(int argc, char** argv) {
 
+  cout << "Success!" << endl;
+  
   return 0;
 }
